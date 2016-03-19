e6d920dde0f789dfef8fbbfc98ba236e1f85a4fc
NaterTots <hodge.nate@gmail.com>
2016-03-19 14:38:28 -0400

    Initial Promote
    This contains some traces of Sloth and DodgeShoot, but for the most part
    it's game agnostic.

---
+++ Assets/UI/TitleScreenCanvas.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TitleScreenCanvas : MonoBehaviour
+{
+    //public bool isMuted = false;
+    //public GameObject muteButton;
+    //public Sprite muteSprite;
+    //public Sprite volumeSprite;
+
+    public void OnStartGame()
+    {
+        Resolver.Instance.GetController<GameStateEngine>().ChangeGameState(GameStateEngine.States.Playing);
+    }
+
+    //public void OnMute()
+    //{
+    //    isMuted = !isMuted;
+
+    //    if (isMuted)
+    //    {
+    //        AudioListener.volume = 0;
+    //        muteButton.GetComponent<Image>().sprite = muteSprite;
+    //    }
+    //    else
+    //    {
+    //        AudioListener.volume = 1;
+    //        muteButton.GetComponent<Image>().sprite = volumeSprite;
+    //    }
+    //}
+
+    public void OnCredits()
+    {
+        Resolver.Instance.GetController<GameStateEngine>().ChangeGameState(GameStateEngine.States.Credits);
+    }
+
+}
+
